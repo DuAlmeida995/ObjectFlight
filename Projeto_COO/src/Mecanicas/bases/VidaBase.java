229ab6323f5235b949baee2d194969df259a5a18
eduardo <11eduardoalmeida@gmail.com>
2025-06-27 13:20:23 -0300

    arrumando conversões

--- Projeto_COO/src/Mecanicas/bases/VidaBase.java
+++ Projeto_COO/src/Mecanicas/bases/VidaBase.java
@@ -8,34 +8,36 @@ import Mecanicas.powerups.Invencibilidade;
 public class VidaBase {
     private int vidaMaxima;
     private int vidaAtual;
-    private boolean invencivel = false; 
+    private boolean invencivel = false;
 
-    private int tempoInvencivel = 0; 
+    private long tempoInvencivel = 0;
     //private int DURACAO_INVENCIVEL = 60;
 
-    public VidaBase(int vidaMaxima){
+    public VidaBase(int vidaMaxima) {
         this.vidaMaxima = vidaMaxima;
         this.vidaAtual = vidaMaxima;
     }
 
-    public int getVidaAtual(){ return this.vidaAtual; }
+    public int getVidaAtual() {
+        return this.vidaAtual;
+    }
 
-    public boolean estaMorto(){
+    public boolean estaMorto() {
         return vidaAtual == 0;
     }
 
-    public boolean estaInvencivel(){
+    public boolean estaInvencivel() {
         return invencivel;
     }
 
-    public void reduzir(){
+    public void reduzir() {
         invencivel = true;
         tempoInvencivel = System.currentTimeMillis() + 1000;
-        vidaAtual --;
-        if(vidaAtual < 0) vidaAtual = 0;
+        vidaAtual--;
+        if (vidaAtual < 0) vidaAtual = 0;
     }
 
-    public void resetar(){
+    public void resetar() {
         vidaAtual = vidaMaxima;
     }
 
@@ -53,29 +55,31 @@ public class VidaBase {
         this.tempoInvencivel = System.currentTimeMillis() + duracao;
     }
 
-    public void drawVidaJogador(){
-        float porcent = (float) vidaAtual/vidaMaxima;
+    public void drawVidaJogador() {
+        float porcent = (float) vidaAtual / vidaMaxima;
 
         GameLib.setColor(Color.gray);
         GameLib.fillRect(80.0, GameLib.HEIGHT - 25, 100.0, 20.0);
 
         GameLib.setColor(Color.RED);
-        GameLib.fillRect(30 + (100 * porcent)/2, GameLib.HEIGHT - 25, 100 * porcent, 20.0);
+        GameLib.fillRect(30 + (100 * porcent) / 2, GameLib.HEIGHT - 25, 100 * porcent, 20.0);
     }
 
-    public void updateInvencibilidade(){
-        if(invencivel && System.currentTimeMillis() > tempoFimInvencibilidade){
+    public void updateInvencibilidade() {
+        if (invencivel && System.currentTimeMillis() > tempoInvencivel) {
             invencivel = false;
         }
-
-    public void drawVidaChefe(){
-        float porcent = (float) vidaAtual/vidaMaxima;
-
-        GameLib.setColor(Color.gray);
-        GameLib.fillRect(240, 130, 360.0, 20.0);
-
-        GameLib.setColor(Color.YELLOW);
-        GameLib.fillRect(60 + (360 * porcent)/2, 130, 360 * porcent, 20.0);
     }
+
+        public void drawVidaChefe () {
+            float porcent = (float) vidaAtual / vidaMaxima;
+
+            GameLib.setColor(Color.gray);
+            GameLib.fillRect(240, 130, 360.0, 20.0);
+
+            GameLib.setColor(Color.YELLOW);
+            GameLib.fillRect(60 + (360 * porcent) / 2, 130, 360 * porcent, 20.0);
+        }
+
 
 }
